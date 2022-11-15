17d657d0e2638a65ff1c96db7d108618cb0a9966
Aditya Ganji <88584574+adityaganji889@users.noreply.github.com>
2022-11-15 22:07:11 +0530

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 const appId = "f3c323f02c1d45ea8d47e930aa9c6920";
-const token = "007eJxTYNDf3nDiZIRl6KmWho0llh/6LG+vSnu5VPHdD5eeOEW/ix8UGNKMk42NjNMMjJINU0xMUxMtUkzMUy2NDRITLZPNLI0MtIIDk5cFMjJcmqLIwAiFID4LQ25iZh4DAwDcGSDu";
+const token = "007eJxTYNDf3nDiZIRl6KmWho0llh/6LG+vSnu5VPHdD5eeOEW/ix8UGNKMk42NjNMMjJINU0xMUxMtUkzMUy2NDRITLZPNLI0MtIIDk5cFMjJcmqLIwAiFID4LQ25iZh4DAwDcGSDu"; //This token get's changed every 12 hours
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
